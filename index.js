367d3760b5beb517460e0e92a884dc51c36d4c8d
Fatime <fatosh127@gmail.com>
2023-11-16 16:14:17 +0400

    Aydan

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import 'react-native-gesture-handler';
 import 'react-native-gesture-handler';
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import Calculator from './src/Calculator';
+import New from './src/New';
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Calculator);
+AppRegistry.registerComponent(appName, () => New);
