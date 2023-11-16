e13c259d13a026142a6a14f42597e14e07fb95bf
Fatime <fatosh127@gmail.com>
2023-11-16 15:35:06 +0400

    calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,124 @@
 
-import React from "react";
-import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+// import React from "react";
+// import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+// import New from "./New";
 
-function App(){
-  return (
-    <SafeAreaView style={styles.area}>
-      <View style={styles.header}>
-        <Image source={require("./assets/icons/menu.png")}/>
-         <View style={{flexDirection:'row',alignItems:'center'}}>
-         <Image style={{marginRight:24}} source={require("./assets/icons/search.png")}/> 
-         <Image source={require("./assets/icons/profile.png")}/>
-         </View> 
-      </View>
-      <View style={styles.welcome}>
-        <Text style={{color:'#000000',fontSize:14,fontWeight:'500',lineHeight:17.07}}>Hello,</Text>
-        <Text style={{fontSize:26,color:'#000000',fontWeight:'700',lineHeight:31.69}}>Lucas Arthur</Text>
-      </View>
-      <View style={styles.categories}>
-        <Text style={{fontSize:14,fontWeight:700,color:'#FF772D',lineHeight:17.07}}>New</Text>
-        <Text style={{fontSize:14,fontWeight:500,color:'#848484',lineHeight:17.07}}>Trendy</Text>
-        <Text style={{fontSize:14,fontWeight:500,color:'#848484',lineHeight:17.07}}>Best Rated</Text>
-      </View>
-      <View>
-      <Image style={{marginVertical:10,marginLeft:55}} source={require("./assets/icons/Ellipse.png")}/>
-      </View>
-      <View>
-      <ScrollView horizontal={true}>
-        <View style={styles.image}>
-        <Image source={require('./assets/icons/images/guitar.png')}/>   
-        <Image style={{marginLeft:13}} source={require('./assets/icons/images/Violin.png')}/>
-        </View>
-      </ScrollView>
-      </View>
-      <View>
-        <Text style={{padding:29, fontSize:18,fontWeight:800,color:'#000000', lineHeight:19.05,}}>Last Seen Courses </Text>
-      </View>
-      <View style={styles.box1}>
-        <View style={styles.imgtext1}>
-        <Image style={{margin:11}}  source={require('./assets/icons/images/Group.png')}/>
-        <View style={styles.texts1}>
-          <Text style={{marginBottom:5,color:'#848484',fontSize:14,fontWeight:500,lineHeight:16,}}>Advanced</Text>
-          <Text style={{marginBottom:7,color:'#545454',fontSize:16,fontWeight:600,lineHeight:20,}}>Beethoven Piano Cycles</Text>
-          <Text style={{color:'#848484',fontSize:14,fontWeight:500,lineHeight:20,}}>30min</Text>
-        </View>
-        </View>
-      </View>
-      <View style={styles.box2}>
-        <View style={styles.imgtext2}>
-        <Image style={{margin:11}}  source={require('./assets/icons/images/Group.png')}/>
-        <View style={styles.texts1}>
-          <Text style={{marginBottom:5,color:'#848484',fontSize:14,fontWeight:500,lineHeight:16,}}>Basic</Text>
-          <Text  style={{marginBottom:7,color:'#545454',fontSize:16.,fontWeight:600,lineHeight:16}}>{"Beginner \nAlto Saxophone"}</Text>
-          <Text style={{color:'#848484',fontSize:14,fontWeight:500,lineHeight:20,}}>45min</Text>
-        </View>
-        </View>
-      </View>
-    </SafeAreaView>
-  )
-}
-export default App
+// function App(){
+//     // return <New/>
+//   return (
+//     <SafeAreaView style={styles.area}>
+//       <View style={styles.header}>
+//         <Image source={require("./assets/icons/menu.png")}/>
+//          <View style={{flexDirection:'row',alignItems:'center'}}>
+//          <Image style={{marginRight:24}} source={require("./assets/icons/search.png")}/> 
+//          <Image source={require("./assets/icons/profile.png")}/>
+//          </View> 
+//       </View>
+//       <View style={styles.welcome}>
+//         <Text style={{color:'#000000',fontSize:14,fontWeight:'500',lineHeight:17.07}}>Hello,</Text>
+//         <Text style={{fontSize:26,color:'#000000',fontWeight:'700',lineHeight:31.69}}>Lucas Arthur</Text>
+//       </View>
+//       <View style={styles.categories}>
+//         <Text style={{fontSize:14,fontWeight:700,color:'#FF772D',lineHeight:17.07}}>New</Text>
+//         <Text style={{fontSize:14,fontWeight:500,color:'#848484',lineHeight:17.07}}>Trendy</Text>
+//         <Text style={{fontSize:14,fontWeight:500,color:'#848484',lineHeight:17.07}}>Best Rated</Text>
+//       </View>
+//       <View>
+//       <Image style={{marginVertical:10,marginLeft:55}} source={require("./assets/icons/Ellipse.png")}/>
+//       </View>
+//       <View>
+//       <ScrollView horizontal={true}>
+//         <View style={styles.image}>
+//         <Image source={require('./assets/icons/images/guitar.png')}/>   
+//         <Image style={{marginLeft:13}} source={require('./assets/icons/images/Violin.png')}/>
+//         </View>
+//       </ScrollView>
+//       </View>
+//       <View>
+//         <Text style={{padding:29, fontSize:18,fontWeight:800,color:'#000000', lineHeight:19.05,}}>Last Seen Courses </Text>
+//       </View>
+//       <View style={styles.box1}>
+//         <View style={styles.imgtext1}>
+//         <Image style={{margin:11}}  source={require('./assets/icons/images/Group.png')}/>
+//         <View style={styles.texts1}>
+//           <Text style={{marginBottom:5,color:'#848484',fontSize:14,fontWeight:500,lineHeight:16,}}>Advanced</Text>
+//           <Text style={{marginBottom:7,color:'#545454',fontSize:16,fontWeight:600,lineHeight:20,}}>Beethoven Piano Cycles</Text>
+//           <Text style={{color:'#848484',fontSize:14,fontWeight:500,lineHeight:20,}}>30min</Text>
+//         </View>
+//         </View>
+//       </View>
+//       <View style={styles.box2}>
+//         <View style={styles.imgtext2}>
+//         <Image style={{margin:11}}  source={require('./assets/icons/images/Group.png')}/>
+//         <View style={styles.texts1}>
+//           <Text style={{marginBottom:5,color:'#848484',fontSize:14,fontWeight:500,lineHeight:16,}}>Basic</Text>
+//           <Text  style={{marginBottom:7,color:'#545454',fontSize:16.,fontWeight:600,lineHeight:16}}>{"Beginner \nAlto Saxophone"}</Text>
+//           <Text style={{color:'#848484',fontSize:14,fontWeight:500,lineHeight:20,}}>45min</Text>
+//         </View>
+//         </View>
+//       </View>
+//     </SafeAreaView>
+//   )
+// }
+// export default App
 
-const styles=StyleSheet.create({
-  area:{
-    flex:1,
-  },
-  header:{
-    flexDirection:'row',
-    margin:25,
-    alignItems:'center',
-    justifyContent:'space-between',
+// const styles=StyleSheet.create({
+//   area:{
+//     flex:1,
+//   },
+//   header:{
+//     flexDirection:'row',
+//     margin:25,
+//     alignItems:'center',
+//     justifyContent:'space-between',
     
-  },
-   welcome:{
-    marginLeft:25,
-    marginTop:32,
-    fontStyle:'Montserrat'
-   },
-   categories:{
-    flexDirection:'row',
-    justifyContent:'space-around',
-    fontStyle:'Montserrat',
-    marginTop:32
-   },
-   image:{
-    flexDirection:'row',
-    marginLeft:35,
-   },
-   box1:{
-    backgroundColor:"#F3F4F9",
-    paddingHorizontal:10,
-    height:187,
-    width:160,
-    marginLeft:25,
-    borderRadius:12,
-   },
-   imgtext1:{
-    flexDirection:'column-reverse',
-    padding:12,
-   },
-   texts1:{
-    fontStyle:'Montserrat',
-   },
-   box2:{
-    backgroundColor:"#F3F4F9",
-    height:187,
-    width:160,
-    marginLeft:230,
-    borderRadius:12,
-    marginTop:-187,
-    paddingHorizontal:10
-   },
-   imgtext2:{
-    flexDirection:'column-reverse',
-    padding:12
-   },
-   texts2:{
-    fontStyle:'Montserrat',
-    margin:16,
-   }
+//   },
+//    welcome:{
+//     marginLeft:25,
+//     marginTop:32,
+//     fontStyle:'Montserrat'
+//    },
+//    categories:{
+//     flexDirection:'row',
+//     justifyContent:'space-around',
+//     fontStyle:'Montserrat',
+//     marginTop:32
+//    },
+//    image:{
+//     flexDirection:'row',
+//     marginLeft:35,
+//    },
+//    box1:{
+//     backgroundColor:"#F3F4F9",
+//     paddingHorizontal:10,
+//     height:187,
+//     width:160,
+//     marginLeft:25,
+//     borderRadius:12,
+//    },
+//    imgtext1:{
+//     flexDirection:'column-reverse',
+//     padding:12,
+//    },
+//    texts1:{
+//     fontStyle:'Montserrat',
+//    },
+//    box2:{
+//     backgroundColor:"#F3F4F9",
+//     height:187,
+//     width:160,
+//     marginLeft:230,
+//     borderRadius:12,
+//     marginTop:-187,
+//     paddingHorizontal:10
+//    },
+//    imgtext2:{
+//     flexDirection:'column-reverse',
+//     padding:12
+//    },
+//    texts2:{
+//     fontStyle:'Montserrat',
+//     margin:16,
+//    }
 
-})
+// })
--- src/New.tsx
+++ src/New.tsx
@@ -1,33 +1,135 @@
-import React from "react";
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+// import React from "react";
+// import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-function New(){
-    return(
-        <SafeAreaView style={styles.area}>
-            <View>
-                <Image style={{marginTop:142,width:412,height:700}} source={require('./assets/icons/images/illustration.png')}/>
-                <Text style={{color:'#FFFFFF',fontSize:37,fontWeight:700,lineHeight:47.88,marginTop:-800,padding:25,}}>Welcome to</Text>
-                <Text style={{color:'#FFA300',fontSize:37,fontWeight:700,lineHeight:46.25,padding:25,marginTop:-37,fontStyle:"normal"}}>MUSIFY</Text>
-                <Text style={{color:'#FFFFFF',fontSize:13,fontWeight:400,lineHeight:24,marginHorizontal:25}}>An interactive music app designed to really</Text>
-                <Text style={{color:'#FFFFFF',fontSize:13,fontWeight:400,lineHeight:24,marginHorizontal:25}}>provide a fully-formed experience to better</Text>
-                <Text style={{color:'#FFFFFF',fontSize:13,fontWeight:400,lineHeight:24,marginHorizontal:25}}>your instrument playing.</Text>
-                <Image style={{margin:25}} source={require('./assets/icons/images/Vector.png')}/>
-                <Text style={{color:'#FFFFFF',fontSize:16,fontWeight:600,lineHeight:19.5,marginLeft:75,marginTop:-55}} >Start Now</Text>
-            </View>
+// function New(){
+
+//     return(
+//         <SafeAreaView style={styles.area}>
+//             <View>
+//                 <Image style={{marginTop:142,width:412,height:700}} source={require('./assets/icons/images/illustration.png')}/>
+//                 <Text style={{color:'#FFFFFF',fontSize:37,fontWeight:700,lineHeight:47.88,marginTop:-800,padding:25,}}>Welcome to</Text>
+//                 <Text style={{color:'#FFA300',fontSize:37,fontWeight:700,lineHeight:46.25,padding:25,marginTop:-37,fontStyle:"normal"}}>MUSIFY</Text>
+//                 <Text style={{color:'#FFFFFF',fontSize:13,fontWeight:400,lineHeight:24,marginHorizontal:25}}>An interactive music app designed to really</Text>
+//                 <Text style={{color:'#FFFFFF',fontSize:13,fontWeight:400,lineHeight:24,marginHorizontal:25}}>provide a fully-formed experience to better</Text>
+//                 <Text style={{color:'#FFFFFF',fontSize:13,fontWeight:400,lineHeight:24,marginHorizontal:25}}>your instrument playing.</Text>
+//                 <Image style={{margin:25}} source={require('./assets/icons/images/Vector.png')}/>
+//                 <Text style={{color:'#FFFFFF',fontSize:16,fontWeight:600,lineHeight:19.5,marginLeft:75,marginTop:-55}} >Start Now</Text>
+//             </View>
 
 
-        </SafeAreaView>
-    )
-}
+//         </SafeAreaView>
+//     )
+// }
 
-export default New
+// export default New
 
-const styles=StyleSheet.create({
-    area:{
-        flex:1,
-        backgroundColor:"#152B79",
+// const styles=StyleSheet.create({
+//     area:{
+//         flex:1,
+//         backgroundColor:"#152B79",
 
      
-    },
+//     },
 
-})
+// })
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { View, Text, Button, StyleSheet } from 'react-native';
+
+// const Calculator = () => {
+//   const [input, setInput] = useState('');
+//   const [result, setResult] = useState('');
+
+//   const handlePress = (value) => {
+//     if (value === '=') {
+//       try {
+//         setResult(eval(input).toString());
+//       } catch (error) {
+//         setResult('Error');
+//       }
+//     } else if (value === 'C') {
+//       setInput('');
+//       setResult('');
+//     } else {
+//       setInput((prevInput) => prevInput + value);
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.resultContainer}>
+//         <Text style={styles.resultText}>{result}</Text>
+//       </View>
+//       <View style={styles.inputContainer}>
+//         <Text style={styles.inputText}>{input}</Text>
+//       </View>
+//       <View style={styles.buttonContainer}>
+//         <View style={styles.row}>
+//           <Button title="7" onPress={() => handlePress('7')} />
+//           <Button title="8" onPress={() => handlePress('8')} />
+//           <Button title="9" onPress={() => handlePress('9')} />
+//           <Button title="/" onPress={() => handlePress('/')} />
+//         </View>
+//         <View style={styles.row}>
+//           <Button title="4" onPress={() => handlePress('4')} />
+//           <Button title="5" onPress={() => handlePress('5')} />
+//           <Button title="6" onPress={() => handlePress('6')} />
+//           <Button title="-" onPress={() => handlePress('-')} />
+//         </View>
+//         <View style={styles.row}>
+//           <Button title="1" onPress={() => handlePress('1')} />
+//           <Button title="2" onPress={() => handlePress('2')} />
+//           <Button title="3" onPress={() => handlePress('3')} />
+//           <Button title="+" onPress={() => handlePress('+')} />
+//         </View>
+//         <View style={styles.row}>
+//           <Button title="C" onPress={() => handlePress('C')} />
+//           <Button title="0" onPress={() => handlePress('0')} />
+//           <Button title="=" onPress={() => handlePress('=')} />
+//           <Button title="*" onPress={() => handlePress('*')} />
+//         </View>
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'flex-end',
+//     padding: 10,
+//   },
+//   resultContainer: {
+//     marginBottom: 10,
+//   },
+//   resultText: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//   },
+//   inputContainer: {
+//     marginBottom: 10,
+//   },
+//   inputText: {
+//     fontSize: 18,
+//   },
+//   buttonContainer: {
+//     width: '100%',
+//   },
+//   row: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     backgroundColor:"aqua",
+//     borderRadius:89,
+//     marginBottom: 70,
+//   },
+// });
+
+// export default Calculator;
