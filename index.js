0d01769969120ad2d8bd9b145cb47450907dae01
Fatime <fatosh127@gmail.com>
2023-12-02 19:50:19 +0400

    Calculator

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import maket2 from './src/maket2';
+import Calculator from './src/Calculator';
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => maket2);
+AppRegistry.registerComponent(appName, () => Calculator);
